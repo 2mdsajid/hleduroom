88d706be1d683256c4f8a57e00d2433f6021fec9
2mdsajid <2mdsajid@gmail.com>
2025-07-06 00:27:09 +0545

    lastttt

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,18 +3,9 @@ import prisma from '@/lib/db';
 import React from 'react'
 
 const Page = async() => {
-  const allUsers = await prisma.user.findMany()
     return (
       <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
         <LoginForm />
-        {allUsers.map((u,i)=>{
-          return (
-            <>
-            <>{u.email}</>
-            <>{u.password}</>
-            </>
-          )
-        })}
       </div>
     );
   };
