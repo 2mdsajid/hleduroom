b8e5cf55b41fc694c874814430bd5404fffc801c
2mdsajid <2mdsajid@gmail.com>
2025-07-05 23:03:40 +0545

    generated freomved from gitit=gnore

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,20 @@
 import LoginForm from '@/components/user-login-form';
+import prisma from '@/lib/db';
 import React from 'react'
 
-const Page = () => {
+const Page = async() => {
+  const allUsers = await prisma.user.findMany()
     return (
       <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center p-4">
         <LoginForm />
+        {allUsers.map((u,i)=>{
+          return (
+            <>
+            <>{u.email}</>
+            <>{u.password}</>
+            </>
+          )
+        })}
       </div>
     );
   };
